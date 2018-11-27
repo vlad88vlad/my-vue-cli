780efd73727269088c20c47ec23437605c4ddf8c
vlad <vlad88vlad@gmail.com>
2018-11-27 13:20:34 +0200

    remove hash name file

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -46,7 +46,8 @@ module.exports = {
                 loader: 'url-loader',
                 options: {
                     limit: 10000,
-                    name: utils.assetsPath('img/[name].[hash:7].[ext]')
+                    // name: utils.assetsPath('img/[name].[hash:7].[ext]')
+                    name: utils.assetsPath('img/[name].[ext]')
                 }
             },
             {
@@ -54,14 +55,16 @@ module.exports = {
                 loader: 'url-loader',
                 options: {
                     limit: 10000,
-                    name: utils.assetsPath('media/[name].[hash:7].[ext]')
+                    // name: utils.assetsPath('media/[name].[hash:7].[ext]')
+                    name: utils.assetsPath('media/[name].[ext]')
                 }
             },
             {
                 test: /\.(ttf|eot|woff|woff2)$/,
                 loader: "url-loader",
                 options: {
-                    name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+                    name: utils.assetsPath('fonts/[name].[ext]')
+                    // name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
 
                     // name: "static/fonts/[name].[ext]",
                 },
